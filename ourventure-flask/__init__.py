0250afa0627475a55bcf7778f589c2a56c800670
jedeland <jedeland@live.co.uk>
2021-10-30 01:43:10 +0200

    Added API endpoints, still early phase

--- ourventure-flask/__init__.py
+++ ourventure-flask/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from Generation import Person
 
 
 #The __init__ function acts as an interface for the API 
 #All non interface files should be imported and presented via __init__
 #Flask does not need to create pages, only API end points to interact with the VUE frontend
 app = Flask(__name__)
+
+@app.route("/region", methods=["GET", "POST"])
+def initialise_region():
+    print("Creating region")
+    return "Got Region"
+
+
+@app.route("/person")
+def get_person():
+    print("")
+    return "Got Person"
 
 
 @app.route("/")
