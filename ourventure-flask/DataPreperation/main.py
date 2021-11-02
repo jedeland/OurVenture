9c19c5dcef3cc1957424035a5e044fc89fe08a60
jedeland <jedeland@live.co.uk>
2021-11-02 17:54:03 +0100

    Added way for wikipedia and wiktionary to be read in tandem, wiki rules for BS4 are the same

--- ourventure-flask/DataPreperation/main.py
+++ ourventure-flask/DataPreperation/main.py
@@ -9,21 +9,23 @@ import requests
 
 def read_wikipedia():
     #https://en.wikipedia.org/wiki/Category:Feminine_given_names
-    url = "https://en.wikipedia.org/wiki/Category:Feminine_given_names"
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, "html.parser")
-    # Returns all subcategory names on wikipedia, need to extract names (e.g. <a title="Category:Vietnamese" extract the cultural name)
-    section = soup.find("div", {"id": "mw-subcategories"}).findAll("div", {"class": "CategoryTreeItem"})
-    print("Section type is : ", type(section))
-    for i in section:
-        sec = i.find("span", {"dir": "ltr"})
-        #print("Section is : ", sec.text)
-        sec = list(map(int, re.findall("\d+", sec.text)))
-        if max(sec) > 50 and "unisex" not in i.a.text.lower():
-            print(i.a.text.lower(), max(sec))
+    urls = ["https://en.wikipedia.org/wiki/Category:Feminine_given_names", "https://en.wiktionary.org/wiki/Category:Female_given_names_by_language"]
+    for url in urls:
+        print(url)
+        html = requests.get(url)
+        soup = BeautifulSoup(html.text, "html.parser")
+        # Returns all subcategory names on wikipedia, need to extract names (e.g. <a title="Category:Vietnamese" extract the cultural name)
+        section = soup.find("div", {"id": "mw-subcategories"}).findAll("div", {"class": "CategoryTreeItem"})
+        print("Section type is : ", type(section))
+        for i in section:
+            sec = i.find("span", {"dir": "ltr"})
+            #print("Section is : ", sec.text)
+            sec = list(map(int, re.findall("\d+", sec.text)))
+            if max(sec) > 50 and "unisex" not in i.a.text.lower():
+                print(i.a.text.lower(), max(sec))
+                
+            #print(sec)
             
-        #print(sec)
-        
     return soup
     
 
