4077b4f089f7d2b6120d7d3c2427caf1740a2524
jedeland <jedeland@live.co.uk>
2021-11-06 21:27:44 +0100

    Added time record to check speed between executes, changed around project structure to use simple list, key value pairs are not necessarily needed as we can read the string start to determine origin

--- ourventure-flask/DataPreperation/main.py
+++ ourventure-flask/DataPreperation/main.py
@@ -2,6 +2,7 @@ import sqlite3
 from bs4 import BeautifulSoup
 import os
 import re
+import time
 from pprint import pprint
 import json
 import requests
@@ -13,10 +14,11 @@ def get_name_targets():
     # https://en.wikipedia.org/wiki/Category:Feminine_given_names
     urls = ["https://en.wikipedia.org/wiki/Category:Feminine_given_names", "https://en.wiktionary.org/wiki/Category:Female_given_names_by_language",
             "https://en.wikipedia.org/wiki/Category:Masculine_given_names", "https://en.wiktionary.org/wiki/Category:Male_given_names_by_language"]
-    viable_links = {urls[0]: [], urls[1]: [], urls[2]: [], urls[3]: []}
+    #viable_links = {urls[0]: [], urls[1]: [], urls[2]: [], urls[3]: []}
+    viable_links = []
     for url in urls:
-        print(url)
-        print(viable_links)
+        #print(url)
+        #print(viable_links)
         html = requests.get(url)
         soup = BeautifulSoup(html.text, "html.parser")
         start_text = url[0:url.find(".org") + 4]
@@ -29,19 +31,21 @@ def get_name_targets():
             sec = list(map(int, re.findall("\d+", sec.text)))
             if max(sec) > 55 and "unisex" not in i.a.text.lower():
                 #print(i.a.text.lower(), max(sec))
-                end_text = start_text + i.a["href"]
-                print(end_text)
-                viable_links[url].append(end_text)
+                final_text = start_text + i.a["href"]
+                #print(final_text)
+                viable_links.append(final_text)
                 #viable_links[url].append(i.a.text.lower().replace("-language", ""))
                 #viable_links.append(i.a.text.lower().replace("-language", ""))
                 #TODO: replace with dict
             #print(sec)
-    pprint(viable_links, width=120, compact=True, sort_dicts=False)
-    print(viable_links[urls[3]][-3])        
+    #pprint(viable_links, width=120, compact=True, sort_dicts=False)
+    #print(viable_links)
+    print(viable_links[-3])        
     return viable_links
     
 def read_targets(values):
     print("Looping over values, looking for names")
+    
 
 
 def read_wiktionary():
@@ -55,6 +59,8 @@ def read_wiktionary():
 if __name__ == '__main__':
     # The BS4 code should read over the wikipedia entries, the wiktionary entries, and one other entry
     # Reads wikipedia into json or df object
+    start = time.time()
     name_values = get_name_targets()
+    print(f"Time taken to read targets ... {time.time() - start} ")
     output_values = read_targets(values=name_values)
     #print(wikipedia_values)
