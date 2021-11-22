d71691ede6287c4c52bf85b1553011119d30a54c
jedeland <jedeland@live.co.uk>
2021-11-22 19:41:18 +0100

    Added comments and refactored some function calls

--- ourventure-flask/DataPreperation/main.py
+++ ourventure-flask/DataPreperation/main.py
@@ -21,9 +21,8 @@ def get_name_targets():
     viable_links = []
     for url in urls:
         # Gets a HTML Object of the url for processing
-        html = requests.get(url)
         # Beatiful soup processes the html objects text value, allowing us to use BeatifulSoup class functions to search through it
-        soup = BeautifulSoup(html.text, "html.parser")
+        soup = get_soup(url)
         # Used to create new URLs found from the next page section, slices the url value from start, up until the .org section eg: https://en.wikipedia.org
         start_text = url[0:url.find(".org") + 4]
 
@@ -113,14 +112,14 @@ def get_name_values(gender_arg, list_arg):
 
         section = soup.find("div", {"id": "mw-pages"})
         if "next page" in section.text:
-            #TODO: add way to follow down the pages
+            #config name data checks if the dictionary needs to be reset for the key value (or initialised), or if no change is needed
             configure_name_data(name_data, origin)
             
             next_link = section.find("a", string="next page")
             print(val, next_link)
-            
+            #Assign names function takes the current section and uses it to update the name_data dictionary
             assign_names(gender_arg, name_data, origin, section, val)
-            
+            #If a next page exists, recursive search is activates, this will search recursively until it find no more hyperlink on the "next page" value
             recursive_search(gender_arg, name_data, val, origin, next_link)
                     
         else:
@@ -131,15 +130,20 @@ def get_name_values(gender_arg, list_arg):
 
 #Header text: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0
 # {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0'}
-@cache
+
 def get_soup(val):
-    html = requests.get(val, headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0'})
-    soup = BeautifulSoup(html.content, "lxml")
+    #Sleep is required so our robot doesnt rush ahead too fast and get shot down by wikipedia
+    time.sleep(0.0055)
+    #Get the value and send user-agent header, pretending like we are making a relevant request from a machine
+    html = requests.get(val, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0'})
+    soup = BeautifulSoup(html.text, "lxml")
+    #Returns the BS4 soup object
     return soup
 
-@cache
+
 def get_recursive_soup(url, href_link):
     # This function searches through all "next pages" that are found for certain values,
+    #href_link is changed to ensure that the ampersand: "&" is output correctly, currently it translates to "&amp"
     href_link = re.sub(r'&amp;', r'&', href_link)
     formed_url = f"{url.split('.org')[0]}.org{href_link}"
     print(formed_url)
@@ -148,9 +152,13 @@ def get_recursive_soup(url, href_link):
     return section
 
 def recursive_search(gender_arg, name_data, val, origin, next_link):
+    #If link from call exists 
     if next_link:
+        #Last found URL for logging
         last_url = re.sub(r'&amp;', r'&', next_link["href"])
+        #Gets soup object of next link
         new_page = get_recursive_soup(val, next_link["href"])
+        #Checks for next page, and looks for hyperlink
         if "next page" in new_page.text:
             next_link = new_page.find("a", string="next page")
             if next_link:
@@ -159,7 +167,9 @@ def recursive_search(gender_arg, name_data, val, origin, next_link):
                 # Call function again using new link
                 recursive_search(gender_arg, name_data, val, origin, next_link)
             else:
+                #Log the last used link in recursive search, for human checking
                 print(f"Last page {last_url}")
+                #Update dict with values from the last page found before recursive loop is broken
                 assign_names(gender_arg, name_data, origin, new_page, last_url)
                     
 
@@ -180,9 +190,7 @@ def get_female_values(female_list):
     female_data = {"name_values": []}
     test_data = {"name_values": {}}
     for f in female_list:
-        html = requests.get(f)
-        soup = BeautifulSoup(html.text, "html.parser")
-        
+        soup = get_soup(f)
         # print(f.split(":", 1)[-1])
         #TODO: Refactor me to look nicer
         origin = re.findall(r"^(.*?)_", f.lower().replace("old_", "").replace("high_", "").replace("langauge_", "").split(":")[-1])[0]
@@ -225,8 +233,6 @@ if __name__ == '__main__':
     female_list = list(filter(lambda k: "_feminine_" in k.lower() or "_female_" in k.lower(), name_values))
     male_list = list(filter(lambda k: "_masculine_" in k.lower() or "_male_" in k.lower(), name_values))
     surname_list = list(filter(lambda k: "_surnames" in k.lower() or "_male_" in k.lower(), name_values))
-
-
     
     
     # print(female_list, "\n\n\n", male_list)
@@ -237,10 +243,13 @@ if __name__ == '__main__':
     #TODO: add last names here
 
     #print(f"Time taken to read targets ... {time.time() - start} ")
-    #pprint(female_vals["name_values"]["spanish"])
-    #pprint(male_vals["name_values"]["spanish"])
+    time.sleep(2)
     # Create json object, and combine dicts 
     output_values = read_targets(female_vals, male_vals, surname_vals)
-    with open("ourventure-flask/DataPreperation/DataCollections/name_collection_output.json", "w") as f:
-        json.dump(output_values, f)
+    pprint(output_values["spanish"])
     
+    with open("ourventure-flask/DataPreperation/DataCollections/name_collection_output.json", "w") as f:
+        json.dump(output_values, f, sort_keys=True, ensure_ascii=False)
+
+    
+    print(f"Time taken to read targets ... {time.time() - start} ")    
