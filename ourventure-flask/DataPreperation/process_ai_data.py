94a0842b33c853eb9828516f5b267ae8a93cf4b8
jedeland <jedeland@live.co.uk>
2022-01-15 21:45:49 +0100

    Initialised script to remove the problematic names using patterns

--- ourventure-flask/DataPreperation/process_ai_data.py
+++ ourventure-flask/DataPreperation/process_ai_data.py
@@ -1,4 +1,35 @@
+import json
 import os
+from traceback import print_list
+import zipfile
 
 
 #TODO: Automate the removal of certain patterns for any future data
+def unpack_files():
+    print(os.getcwd())
+    path = f"{os.getcwd()}/AI_Data"
+    for i in os.listdir(path):
+        if i.endswith(".zip"):
+            print(i)
+            with zipfile.ZipFile(f"{path}/{i}", "r") as zip:
+                zip.extractall(f"{path}")
+
+def get_wiki_data():
+    if os.path.exists("DataCollections/name_collection_latin.json"):
+        print("Names exists")
+        with open("DataCollections/name_collection_latin.json", "r") as json_file:
+            names_file = json.load(json_file)
+            print(names_file.keys())
+    elif not os.path.exists("DataCollections/name_collection_latin.json"):
+        print("Please run the 'unzipper' file first")
+    
+    #TODO: create hashmap linking dict values to csv values
+
+def load_names():
+    print()
+
+
+if __name__ == "__main__":
+    unpack_files()
+    get_wiki_data()
+    load_names()
