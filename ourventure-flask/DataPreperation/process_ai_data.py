ea6cc2752973e0b39e269f0ae0c8dbc299dabd04
Jed Eland <jedeland@live.co.uk>
2022-01-20 16:24:35 +0100

    Added todo, decided hash map was better

--- ourventure-flask/DataPreperation/process_ai_data.py
+++ ourventure-flask/DataPreperation/process_ai_data.py
@@ -2,38 +2,50 @@ import json
 import os
 from traceback import print_list
 import zipfile
+import country_converter as coco
 
 
 #TODO: Automate the removal of certain patterns for any future data
 def unpack_files():
     print(os.getcwd())
-    path = f"{os.getcwd()}\AI_Data"
+    path = f"{os.getcwd()}\DataPreperation\AI_Data"
     
     for i in os.listdir(path):
         if i.endswith(".zip"):
-            print(i)
-            with zipfile.ZipFile(f"{path}/{i}", "r") as zip:
+            # print(i)
+            with zipfile.ZipFile(f"{path}\{i}", "r") as zip:
                 zip.extractall(f"{path}")
 
 def get_wiki_data():
-    if os.path.exists("DataCollections/name_collection_latin.json"):
+    if os.path.exists("DataPreperation/DataCollections/name_collection_latin.json"):
         print("Names exists")
-        with open("DataCollections/name_collection_latin.json", "r") as json_file:
+        with open("DataPreperation/DataCollections/name_collection_latin.json", "r", encoding="utf-8") as json_file:
             names_file = json.load(json_file)
             print(names_file.keys())
             return names_file
-    elif not os.path.exists("DataCollections/name_collection_latin.json"):
+    elif not os.path.exists("DataPreperation/DataCollections/name_collection_latin.json"):
         print("Please run the 'unzipper' file first")
     
-    #TODO: create hashmap linking dict values to csv values
 
-def identify_names(input_file):
+def identify_names(input_file_wiki):
     print("Finding overlaps")
-    print(input_file)
-    
+    # print(input_file_wiki)
+    #TODO: use coco converter to check overlap
+    #TODO: create map for name + location eg {basque: spain, arabic: egypt x y z, }
+    shared_values = []
+    wiki_keys = input_file_wiki.keys()
+    print(type(wiki_keys))
+    print(wiki_keys)
+    wiki_keys_converted = coco.convert(names=wiki_keys, to="name_short")
+    print(wiki_keys_converted)
+    for i in os.listdir(f"{os.getcwd()}\DataPreperation\AI_Data"):
+        print(i)
+        if i.lower().split(".")[0] in input_file_wiki.keys() and i:
+            print(i)
 
 
 if __name__ == "__main__":
     unpack_files()
     results = get_wiki_data()
-    identify_names(results)
+    # print(results)
+    shared_names = identify_names(results)
